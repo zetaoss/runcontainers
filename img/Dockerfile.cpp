90139d7c701a909ef28013f78960c265bb19f225
Jmnote <opcore@gmail.com>
2024-12-17 15:09:52 +0000

    cpp test

--- img/Dockerfile.cpp
+++ img/Dockerfile.cpp
@@ -1,13 +1,20 @@
+# main
 ARG DEBIAN_TAG=latest
-FROM debian:${DEBIAN_TAG}
+FROM debian:${DEBIAN_TAG} AS main
 
 RUN useradd -m -d /home/user01 user01 \
     && apt-get update && apt-get install -y --no-install-recommends \
-    build-essential \
-    gcc \
+    g++ \
     libc6-dev \
-    time \
     && rm -rf /var/lib/apt/lists/*
 
 USER user01
 WORKDIR /home/user01/
+
+## test
+FROM main AS test
+RUN echo '#include <iostream>' > hello.cpp \
+    && echo 'int main() {std::cout<<"hello"<<std::endl;}' >> hello.cpp \
+    && g++ hello.cpp && ./a.out
+
+FROM main
