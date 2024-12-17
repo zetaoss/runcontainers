ebc4e1cd27844cbb3071280686a74bd24cbb2c6e
Jmnote <opcore@gmail.com>
2024-12-17 15:12:17 +0000

    cpp test

--- img/Dockerfile.cpp
+++ img/Dockerfile.cpp
@@ -5,7 +5,6 @@ FROM debian:${DEBIAN_TAG} AS main
 RUN useradd -m -d /home/user01 user01 \
     && apt-get update && apt-get install -y --no-install-recommends \
     g++ \
-    libc6-dev \
     && rm -rf /var/lib/apt/lists/*
 
 USER user01
