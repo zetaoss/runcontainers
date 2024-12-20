2cd4e6e1455ead54ef06bf95eba27dbbc50df68d
Jmnote <opcore@gmail.com>
2024-12-20 17:04:18 +0000

    java, sqlite3

--- img/Dockerfile.java
+++ img/Dockerfile.java
@@ -1,24 +1,19 @@
 FROM gradle:jdk21 AS gradle
-RUN set -x \
+RUN set -eux \
+    && useradd -m -d /home/user01 user01 \
+    && apt-get update && apt-get install -y --no-install-recommends \
+    asciinema \
+    fonts-nanum-coding \
+    && rm -rf /var/lib/apt/lists/* \
+    && mkdir -p /demo/src /demo/bin \
+    && cd /demo \
     && echo "plugins{id'application'};repositories{mavenCentral()};task getDeps(type:Copy){from sourceSets.main.runtimeClasspath; into 'lib/'};dependencies {" > build.gradle \
     && echo "implementation 'com.fasterxml.jackson.core:jackson-databind:2.18.2'" >> build.gradle \
     && echo "implementation 'com.google.guava:guava:33.3.1-jre'" >> build.gradle \
     && echo "implementation 'com.google.code.gson:gson:2.11.0'" >> build.gradle \
     && echo "}" >> build.gradle \
-    && gradle getDeps
-
-FROM eclipse-temurin:21
-COPY --from=gradle /home/gradle/lib /demo/lib
-
-RUN useradd -m -d /home/user01 user01 \
-    && mkdir -p /demo/src /demo/bin \
-    && chown -R user01:user01 /demo \
-    && apt-get update && apt-get install -y --no-install-recommends \
-    asciinema \
-    fonts-nanum-coding \
-    time \
-    unzip \
-    && rm -rf /var/lib/apt/lists/*
+    && gradle getDeps \
+    && chown -R user01:user01 /demo
 
 USER user01
 WORKDIR /demo
